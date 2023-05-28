874ce2683df9f6062d7f7bf1b1a49b2a44978a8f
Roi Solomon <roisol144@gmail.com>
2023-05-28 07:50:22 +0300

    Added another header

---
+++ Checkers-Project1/game.c
@@ -0,0 +1,4 @@
+#include "game.h"
+
+//void Turn(Board board, Player player);
+//void PlayGame(Board board, Player starting_player);
---
+++ Checkers-Project1/game.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Board.h"
+#include "movesList.h"
+#include "movesTree.h"
+
+
+//void Turn(Board board, Player player);
+
+//void PlayGame(Board board, Player starting_player);
